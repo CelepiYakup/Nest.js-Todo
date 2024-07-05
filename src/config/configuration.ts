2a4d6f894cf030beb8469ccd25134c2048264fc1
CelepiYakup <yakupcelepi56@gmail.com>
2024-07-05 11:17:23 +0300

    configuration file added.

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT,
+    REDIS_NAME: process.env.REDIS_NAME,
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+});
+
