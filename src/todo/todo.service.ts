40bafc239aef96f113801dfd76ae58e7a08e0f5f
CelepiYakup <yakupcelepi56@gmail.com>
2024-07-05 23:22:22 +0300

    todo service update

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
+import { CreateTodoDto } from './dto/create-todo.dto/create-todo.dto';
 
 
 @Injectable()
 export class TodoService {
   constructor( private readonly databaseService: DatabaseService ) {}
 
-  create(createTodoDto) {
-    return 'This action adds a new todo';
+  create(createTodoDto: CreateTodoDto) {
+    const newTodo = { id: Date.now(), ...createTodoDto };
+    this.databaseService.redis.set(newTodo.id.toString(), JSON.stringify(newTodo));
+    return newTodo;
   }
   
 
@@ -20,7 +23,9 @@ export class TodoService {
     return `This action returns a #${id} todo`;
   }
 
-  update(id: number, updateTodoDto) {
+  update(id: number, updateTodoDto: Partial<CreateTodoDto>) {
+    
+    console.log(updateTodoDto);
     return `This action updates a #${id} todo`;
   }
 
